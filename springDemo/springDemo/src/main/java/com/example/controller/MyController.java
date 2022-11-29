46641ecd3e5114a9d175b978b19f0c38d6a4941d
pawalepayal <payalpawale1810@gamil.com>
2022-11-29 14:33:34 +0530

    added naming convensions

--- springDemo/springDemo/src/main/java/com/example/controller/MyController.java
+++ springDemo/springDemo/src/main/java/com/example/controller/MyController.java
@@ -19,29 +19,28 @@ public class MyController {
     }
 
     @GetMapping("/account")
-    public List<Account> getAccounts() {
-        return this.service.getAccounts();
+    public List<Account> getAllAccounts() {
+        return service.getAllAccounts();
     }
 
     @GetMapping("/account/get/{accountId}")
-    public Account getAccount(@PathVariable String accountId) {
-        return this.service.getAccount(Integer.parseInt(accountId));
+    public Account getAccountByAccoundNumber(@PathVariable long accoundNumber) {
+        return service.getAccountByAccountNumber(accoundNumber);
     }
 
-    @PostMapping("/account/add") //to add
+    @PostMapping("/account/add")
     public List<Account> addAccount(@RequestBody Account account) {
-
-        return this.service.addAccount(account);
+        return service.addAccount(account);
     }
 
-    @PutMapping("/account/update/{accountNumber}")//to update
+    @PutMapping("/account/update/{accountNumber}")
     public Account updateAccount(@PathVariable long accountNumber, @RequestBody Account account) {
-        return this.service.updateAccount(accountNumber, account);
+        return service.updateAccount(accountNumber, account);
     }
 
     @DeleteMapping("/account/delete/{accountNumber}")
     public  String deleteAccount(@PathVariable long accountNumber) {
-        return this.service.deleteAccount(accountNumber);
+        return service.deleteAccount(accountNumber);
     }
 
 }
--- springDemo/springDemo/src/main/java/com/example/service/Service.java
+++ springDemo/springDemo/src/main/java/com/example/service/Service.java
@@ -5,10 +5,8 @@ import com.example.entities.Account;
 import java.util.List;
 
 public interface Service {
-    public List<Account> getAccounts();
-
-    Account getAccount(long accountNumber);
-
+    public List<Account> getAllAccounts();
+    Account getAccountByAccountNumber(long accountNumber);
     public List<Account> addAccount(Account account);
     String deleteAccount(long accountNumber);
     Account updateAccount(long accountNumber,Account account);
